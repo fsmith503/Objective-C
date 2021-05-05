c6d0017423570ef22bc9ffcc7e5d1fef0caf99c2
Franklin Smith <franklin@Mac-306C38.attlocal.net>
2021-05-04 19:23:34 -0700

    udpate

--- BMITime/BMITime/main.m
+++ BMITime/BMITime/main.m
@@ -14,13 +14,15 @@ int main(int argc, const char * argv[]) {
         
         
         // create an instance of BNREmployee
-        BNRPerson *mikey = [[BNREmployee alloc] init];
+        BNREmployee *mikey = [[BNREmployee alloc] init];
         
         // give the instance variables interesting values using setters
 //        [mikey setWeightInKilos:96];
 //        [mikey setHeightInMeters:1.8];
         mikey.weightInKilos = 96;
         mikey.heightInMeters = 1.8;
+        mikey.employeeID = 12;
+        mikey.hireDate = [NSDate dateWithNaturalLanguageString:@"Aug 2nd, 2010"];
         
         // log the instance variables using the getters
 //        float height = [mikey heightInMeters];
@@ -29,9 +31,11 @@ int main(int argc, const char * argv[]) {
         int weight = mikey.weightInKilos;
         
         NSLog(@"Mike is %.2f tall and %d weight in kilos", height, weight);
+        NSLog(@"Employee %lu hired on %@", mikey.employeeID, mikey.hireDate);
         
         // log some values using custom methods
         float bmw = [mikey bodyMassindex];
+        double years = [mikey yearsOfEmployment];
         NSLog(@"Mikey has BMI of %.2f", bmw);
         
         
